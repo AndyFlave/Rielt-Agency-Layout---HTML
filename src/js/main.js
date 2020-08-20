bfa780ef6c6c819996f7aabb7b3df494f5f10fae
AndyFlave <35171083+AndyFlave@users.noreply.github.com>
2020-08-20 21:45:22 +0300

    header HTML created, header initial styles, mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
-$(function () {});
+$(function () {
+	let hamburger = $('.hamburger');
+
+	function toogleClassHamburger() {
+		hamburger.toggleClass('active')
+	}
+
+	function toggleClassHtml() {
+		$('html').toggleClass('active');
+	}
+
+	function toggleMenu() {
+		toogleClassHamburger();
+		toggleClassHtml();
+	}
+
+	function closeMenu() {
+		if ($(window).width() >= 768) {
+			hamburger.removeClass('active')
+			$('html').removeClass('active');
+		}
+	}
+
+	hamburger.on('click', toggleMenu);
+
+	$(window).resize(closeMenu);
+});
